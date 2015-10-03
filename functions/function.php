a0d9d84b70993e6d67be56a3bf77726d6df732ae
vkopytich <kopytich.vitaly@gmail.com>
2015-10-03 23:54:05 +0300

    Edit function.php

--- functions/function.php
+++ functions/function.php
@@ -38,49 +38,6 @@ function addtocart($goods_id){
 }
 
 //Pagination
-function pagination($page, $pages_count){
-    if($_SERVER['QUERY_STRING']){
-        $uri = "";
-        foreach($_GET as $key => $value){
-            if($key != 'page') $uri .= "{$key}={$value}&amp;";
-        }
-    }
 
-    $back = '';
-    $forward = '';
-    $startpage = '';
-    $endpage = '';
-    $page2left = '';
-    $page1left = '';
-    $page2right = '';
-    $page1right = '';
-
-    if($page > 1){
-        $back = "<a href='?{$uri}page=".($page-1)."' class='nav_link'>&lt;</a>";
-    }
-    if($page < $pages_count){
-        $forward = "<a href='?{$uri}page=".($page+1)."' class='nav_link'>&gt;</a>";
-    }
-    if($page > 3){
-        $startpage = "<a href='?{$uri}page=1' class='nav_link'>&laquo;</a>";
-    }
-    if($page < ($pages_count - 2)){
-        $endpage = "<a href='?{$uri}page={$pages_count}' class='nav_link'>&raquo;</a>";
-    }
-    if($page - 2 > 0){
-        $page2left = "<a href='?{$uri}page=".($page-2)."' class='nav_link'>".($page-2)."</a>";
-    }
-    if($page - 1 > 0){
-        $page1left = "<a href='?{$uri}page=".($page-1)."' class='nav_link'>".($page-1)."</a>";
-    }
-    if($page + 2 <= $pages_count){
-        $page2right = "<a href='?{$uri}page=".($page+2)."' class='nav_link'>".($page+2)."</a>";
-    }
-    if($page + 1 <= $pages_count){
-        $page1right = "<a href='?{$uri}page=".($page+1)."' class='nav_link'>".($page+1)."</a>";
-    }
-
-    echo '<div class="pagination">'.$startpage.$back.$page2left.$page1left.'<a class="nav_active">'.$page.'</a>'.$page1right.$page2right.$forward.$endpage.'</div>';
-}
 
 // <<  <  1 | 2 | 3 | 4 | 5 | 6  >  >>
